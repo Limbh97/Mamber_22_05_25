3192689efa97f790974d2e0212b9ad1966248d2b
Limbh97 <usr852963@naver.com>
2022-05-25 17:56:08 +0900

    Initial commit

---
+++ src/main/java/com/example/mamber_22_05_25/Mamber220525Application.java
@@ -0,0 +1,13 @@
+package com.example.mamber_22_05_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mamber220525Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mamber220525Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mamber_22_05_25/Mamber220525ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mamber_22_05_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mamber220525ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
